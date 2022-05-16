9a3e7b64b0e038fcdf612ee4db164b8c81afd597
S14mx <sergey.otryshko@gmail.com>
2022-05-15 18:15:06 -0700

    fixed bugs, added functionality for stretch goals

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -11,17 +11,19 @@ intro_body = """
 intro_end = new_line + intro_start
 
 menu = {
-    "Appetizers": {"Wings": 0, "Cookies": 0, "Spring Rolls": 0},
-    "Entrees": {"Salmon": 0, "Steak": 0, "Meat Tornado": 0, "A Literal Garden": 0},
-    "Deserts": {"Ice Cream": 0, "Cake": 0, "Pie": 0},
-    "Drinks": {"Coffee": 0, "Tea": 0, "Unicorn Tears": 0}
+    "Appetizers": ["Wings", "Cookies", "Spring Rolls"],
+    "Entrees": ["Salmon", "Steak", "Meat Tornado", "A Literal Garden"],
+    "Deserts": ["Ice Cream", "Cake", "Pie"],
+    "Drinks": ["Coffee", "Tea", "Unicorn Tears"]
 }
+
+order = {}
 
 print(intro_start, intro_body, intro_end)
 
 for section in menu.keys():
     print(new_line + section + new_line + "-" * len(section))
-    for item in menu[section].keys():
+    for item in menu[section]:
         print(item)
 
 print(new_line + "*" * 35, new_line + "** What would you like to order? **" + new_line + "*" * 35)
@@ -32,12 +34,23 @@ user_input = input("> ").title()
 def add_to_order(user_input):
     for section in menu:
         if user_input in menu[section]:
-            menu[section][user_input] += 1
-            print(f"** {menu[section][user_input]} {'order' if menu[section][user_input] == 1 else 'orders'} of {user_input} {'has' if menu[section][user_input] == 1 else 'have' } been added to your meal **")
+            if user_input in order:
+                order[user_input] += 1
+            else:
+                order[user_input] = 1
+            print(f"** {order[user_input]} {'order' if order[user_input] == 1 else 'orders'} of {user_input} {'has' if order[user_input] == 1 else 'have' } been added to your meal **")
             break
     else:
-        print(f"Sorry, we dont have {user_input} on the menu")
+        if user_input in order:
+            order[user_input] += 1
+        else:
+            order[user_input] = 1
+        print(f"** We dont have {user_input} on the menu but we will make it happen! **")
+
 
 while user_input != "quit":
     add_to_order(user_input)
+    print("Your total order is:")
+    for item in order:
+        print(f" {item}" + f" {order[item]}")
     user_input = input("> ").title()
